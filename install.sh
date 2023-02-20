a761ccc89830abbc0d35d9306792fd61ad1aed68
Emiliano Córdoba <31087818+emicba@users.noreply.github.com>
2023-02-19 22:45:58 -0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+function __vim() {
+  # https://github.com/neovim/neovim/wiki/Building-Neovim#quick-start
+  sudo apt update
+  sudo apt-get install ninja-build gettext libtool libtool-bin cmake g++ pkg-config unzip curl doxygen
+  git submodule update --init --recursive
+  pushd neovim
+  make CMAKE_BUILD_TYPE=Release
+  sudo make install
+  popd
+}
+
+if [[ -z "__$1" || "$(type -t __$1)" != "function" ]]; then
+  echo "Usage: $0 [target]"
+  exit 1
+else
+  set -x
+  "__$1"
+fi
