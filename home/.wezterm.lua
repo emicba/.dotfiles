e11c8038dfa0ba4f995d8d7af96b8dc26220e830
Emiliano Córdoba <31087818+emicba@users.noreply.github.com>
2023-08-29 20:52:04 -0300

    update wezterm config

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -1,7 +1,23 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
-local keys = {
+local function array_concat(...)
+  local ret = {}
+  for _, arr in ipairs { ... } do
+    for _, v in ipairs(arr) do
+      table.insert(ret, v)
+    end
+  end
+  return ret
+end
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.keys = {
   {
     key = 't',
     mods = 'CTRL',
@@ -18,14 +34,14 @@ local keys = {
 }
 
 for i = 1, 8 do
-  table.insert(keys, {
+  table.insert(config.keys, {
     key = tostring(i),
     mods = 'CTRL',
     action = act.ActivateTab(i - 1),
   })
 end
 
-local mouse_bindings = {
+config.mouse_bindings = {
   -- Scrolling up while holding CTRL increases the font size
   {
     event = { Down = { streak = 1, button = { WheelUp = 1 } } },
@@ -53,8 +69,7 @@ local mouse_bindings = {
   },
 }
 
-local hyperlink_rules = {
-  -- Linkify things that look like URLs and the host has a TLD name.
+config.hyperlink_rules = array_concat(wezterm.default_hyperlink_rules(), {
   {
     regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
     format = '$0',
@@ -71,9 +86,9 @@ local hyperlink_rules = {
     regex = [[\bhttp://localhost(:\d{1,5})?\S*\b]],
     format = '$0',
   },
-}
+})
 
-local font_config = {
+config.font = wezterm.font_with_fallback {
   { family = 'Cascadia Mono', weight = 'DemiLight' },
   {
     family = 'JetBrains Mono',
@@ -81,18 +96,12 @@ local font_config = {
   },
 }
 
-local config = {
-  adjust_window_size_when_changing_font_size = false,
-  check_for_updates = false,
-  color_scheme = 'Dracula',
-  font = wezterm.font_with_fallback(font_config),
-  font_size = 14,
-  hyperlink_rules = hyperlink_rules,
-  initial_cols = 120,
-  initial_rows = 30,
-  keys = keys,
-  max_fps = 75,
-  mouse_bindings = mouse_bindings,
-}
+config.adjust_window_size_when_changing_font_size = false
+config.check_for_updates = false
+config.color_scheme = 'Dracula'
+config.font_size = 14
+config.initial_cols = 120
+config.initial_rows = 30
+config.max_fps = 75
 
 return config
